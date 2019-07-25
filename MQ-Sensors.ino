d2117347db2e8cc703773747038e969d94e16883
Anthony Parra <anthony.parrapro@gmail.com>
2019-07-25 14:35:00 -0700

    Add files via upload

--- MQ-Sensors.ino
+++ MQ-Sensors.ino
@@ -1,7 +1,9 @@
+
 //---------------------------------------------------------------------------------------------------------------
 //                                                  LIBRARIES
 //---------------------------------------------------------------------------------------------------------------
 
+#include <MQ2.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
@@ -9,9 +11,19 @@
 //                                                  DEFINES
 //---------------------------------------------------------------------------------------------------------------
 
-#define anInput     A0  
 #define co2Zero     55  
 #define led          9
+#define         MQ_PIN                       (A1)     //define which analog input channel you are going to use
+#define         RL_VALUE                     (10)    //define the load resistance on the board, in kilo ohms
+#define         RO_CLEAN_AIR_FACTOR          (9.21)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
+                                                     //cablibration phase
+#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in 
+                                                     //normal operation                                                    //which is derived from the chart in datasheet
+#define         GAS_H2                      (0)
+ 
 
 //---------------------------------------------------------------------------------------------------------------
 //                                                  LIBRARY CALLS
@@ -22,14 +34,39 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);         // Set the LCD address to 0x27 for a
 //---------------------------------------------------------------------------------------------------------------
 //                                                  SETUP
 //---------------------------------------------------------------------------------------------------------------
+float           H2Curve[3]  =  {2.3, 0.93,-1.44};    //two points are taken from the curve in datasheet. 
+                                                     //with these two points, a line is formed which is "approximately equivalent" 
+                                                     //to the original curve. 
+                                                     //data format:{ x, y, slope}; point1: (lg200, lg8.5), point2: (lg10000, lg0.03) 
+ 
+float           Ro           =  10;                  //Ro is initialized to 10 kilo ohms
+
+int Analog_Input = A0;
+int lpg, co, smoke;
+
+MQ2 mq2(Analog_Input);
+
 void setup()
 {
   Serial.begin(9600);           //Opening Serial port
+
+  lcd.begin();                  // initialize the LCD
+  lcd.backlight();              // Turn on the blacklight and print a message.
+  lcd.print("Calibrating...");
   
-	
-	lcd.begin();                  // initialize the LCD
-	lcd.backlight();              // Turn on the blacklight and print a message.
-	
+  Serial.print("Calibrating...\n");                
+  Ro = MQCalibration(MQ_PIN);                        //Calibrating the sensor. Please make sure the sensor is in clean air 
+                                                     //when you perform the calibration                    
+  Serial.print("Calibration is done...\n"); 
+  Serial.print("Ro=");
+  Serial.print(Ro);
+  Serial.print("kohm");
+  Serial.print("\n");
+
+  mq2.begin();
+
+  lcd.clear();
+  
 }
 
 void loop()
@@ -60,17 +97,126 @@ for (int x = 0;x<10;x++){                     //add samples together
 
   lcd.setCursor(0,0);                         //set cursor
   lcd.print("CO2 Level:");                    //print title
+  Serial.print("CO2 Level:");
   lcd.print(co2ppm);                          //print co2 ppm
+  Serial.print(co2ppm);
   lcd.print("PPM");                           //print units
-  lcd.setCursor(0,1); 
+  Serial.print("PPM");
+  //lcd.setCursor(0,1); 
 
+  lcd.setCursor(0,1);
+  Serial.print("H2 Concentration:"); 
+  lcd.print("H2 Level:");
+  Serial.print(MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_H2) );
+  lcd.print(MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_H2) );
+  Serial.print( "ppm" );
+  lcd.print("ppm");
+  Serial.print("\n"); 
+  //lcd.setCursor(0,1);
+
+  float* values= mq2.read(false); //set it false if you don't want to print the values in the Serial
+  //lpg = values[0];
+  lpg = mq2.readLPG();
+  //co = values[1];
+  co = mq2.readCO();
+  //smoke = values[2];
+  smoke = mq2.readSmoke();
+
+  Serial.print("LPG: ");
+  Serial.print(lpg);
+  Serial.print("PPM \n");
+  Serial.print("CO: ");
+  Serial.print(co);
+  Serial.print("PPM \n");
+  Serial.print("SMOKE: ");
+  Serial.print(smoke);
+  Serial.print("PPM \n");
   
-  delay(100);
-  if(co2ppm>999){                             //if co2 ppm > 1000
-    digitalWrite(led,HIGH);                   //turn on led
-  }
-  else{                                       //if not
-    digitalWrite(led,LOW);                    //turn off led
-  }
+  delay(1000);
   
+  
+  
+}
+
+/****************** MQResistanceCalculation ****************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/ 
+float MQResistanceCalculation(int raw_adc) {
+  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+}
+ 
+/***************************** MQCalibration ****************************************
+Input:   mq_pin - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use  
+         MQResistanceCalculation to calculates the sensor resistance in clean air 
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about 
+         10, which differs slightly between different sensors.
+************************************************************************************/ 
+float MQCalibration(int mq_pin) {
+  int i;
+  float val=0;
+ 
+  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+    val += MQResistanceCalculation(analogRead(mq_pin));
+    delay(CALIBRATION_SAMPLE_INTERVAL);
+  }
+  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+ 
+  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro 
+                                                        //according to the chart in the datasheet 
+ 
+  return val; 
+}
+/*****************************  MQRead *********************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/ 
+float MQRead(int mq_pin) {
+  int i;
+  float rs=0;
+ 
+  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    rs += MQResistanceCalculation(analogRead(mq_pin));
+    delay(READ_SAMPLE_INTERVAL);
+  }
+ 
+  rs = rs/READ_SAMPLE_TIMES;
+ 
+  return rs;  
+}
+ 
+/*****************************  MQGetGasPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function passes different curves to the MQGetPercentage function which 
+         calculates the ppm (parts per million) of the target gas.
+************************************************************************************/ 
+int MQGetGasPercentage(float rs_ro_ratio, int gas_id) {
+  if ( gas_id == GAS_H2) {
+     return MQGetPercentage(rs_ro_ratio,H2Curve);
+  }  
+  return 0;
+}
+ 
+/*****************************  MQGetPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         pcurve      - pointer to the curve of the target gas
+Output:  ppm of the target gas
+Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm) 
+         of the line could be derived if y(rs_ro_ratio) is provided. As it is a 
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic 
+         value.
+************************************************************************************/ 
+int  MQGetPercentage(float rs_ro_ratio, float *pcurve) {
+  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
 }
